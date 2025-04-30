371a82594aa84ed5572ae1d1ec1ac60b82ed2c13
rosa <rosarioaurelia09@gmail.com>
2025-04-30 12:37:22 +0700

    update create activity 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Poppins',
 
       ),
-      home: GuruReportMainPage(),
+      home: SplashScreen(),
       routes: {
         '/home': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
       },
