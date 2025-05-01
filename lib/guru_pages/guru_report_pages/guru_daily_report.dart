01f297608bf6e6e4843a4183828b0521c39d5524
Muhammad Helga <muhammadhelga41@gmail.com>
2025-05-01 13:25:54 +0700

    rebuild ui

--- lib/guru_pages/guru_report_pages/guru_daily_report.dart
+++ lib/guru_pages/guru_report_pages/guru_daily_report.dart
@@ -13,14 +13,14 @@ class _GuruDailyReportPageState extends State<GuruDailyReportPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:AppColors.primary5,
-        appBar: AppBar(
+      backgroundColor: AppColors.primary5,
+      appBar: AppBar(
         backgroundColor: AppColors.primary50,
         elevation: 0,
         title: Text(
           'Laporan Harian',
           style: TextStyle(
-            fontSize: 20,
+            fontSize: 30,
             fontWeight: FontWeight.w600,
             color: AppColors.primary5,
           ),
@@ -36,7 +36,7 @@ class _GuruDailyReportPageState extends State<GuruDailyReportPage> {
             child: Icon(
               Icons.chevron_left,
               color: AppColors.primary50,
-              size: 22,
+              size: 38,
             ),
           ),
           onPressed: () {
--- lib/guru_pages/guru_report_pages/guru_weekly_report.dart
+++ lib/guru_pages/guru_report_pages/guru_weekly_report.dart
@@ -13,14 +13,14 @@ class _GuruWeeklyReportPageState extends State<GuruWeeklyReportPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:AppColors.primary5,
-        appBar: AppBar(
+      backgroundColor: AppColors.primary5,
+      appBar: AppBar(
         backgroundColor: AppColors.primary50,
         elevation: 0,
         title: Text(
           'Laporan Harian',
           style: TextStyle(
-            fontSize: 20,
+            fontSize: 30,
             fontWeight: FontWeight.w600,
             color: AppColors.primary5,
           ),
@@ -36,7 +36,7 @@ class _GuruWeeklyReportPageState extends State<GuruWeeklyReportPage> {
             child: Icon(
               Icons.chevron_left,
               color: AppColors.primary50,
-              size: 22,
+              size: 38,
             ),
           ),
           onPressed: () {
