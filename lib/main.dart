cfdf80900b4c450052c17df80ead7e5377b3b83f
rosa <rosarioaurelia09@gmail.com>
2025-05-02 14:40:23 +0700

    update create activity 5.0

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         fontFamily: 'Poppins',
       ),
-      home: GuruReportMainPage(role: '',),
+      home: SplashScreen(),
       routes: {
         '/home': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
       },
